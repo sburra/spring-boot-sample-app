33488783063234c91803f3c25a07e8778ed59d34
Srinivas <32594526+sburra@users.noreply.github.com>
2018-08-07 13:50:13 +0530

    Update Application.java

--- src/main/java/com/test/sampleapp/Application.java
+++ src/main/java/com/test/sampleapp/Application.java
@@ -9,11 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
-public class Application1 {
+public class Application {
 
     public static void main(String[] args) {
         final SpringApplication springApplication =
-            new SpringApplication(Application1.class);
+            new SpringApplication(Application.class);
         springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
     }
